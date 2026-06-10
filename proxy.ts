4651ec6d726691c85a57e4cf1fe17083eef5d5b7
bimbaim <baim.webdev@gmail.com>
2026-06-10 11:33:55 +0700

    feat: implement Supabase session management middleware for request authentication and redirection

---
+++ proxy.ts
@@ -0,0 +1,19 @@
+import { updateSession } from "@/lib/supabase/proxy";
+import { type NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - All files with extensions (e.g. widget.js, images, etc.)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.[a-zA-Z0-9]+$).*)",
+  ],
+};
